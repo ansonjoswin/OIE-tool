2c257dfbbbe000a3f09f7050c7308daa07d6c89b
kavya <ksathyanarayana@unomaha.edu>
2017-02-18 11:50:18 -0600

    Fixing seeder , removing roles - student , staff

--- database/seeds/UsersRolesPermissions.php
+++ database/seeds/UsersRolesPermissions.php
@@ -64,12 +64,12 @@ class RoleUserTableSeeder extends Seeder {
         DB::table('role_user')->insert($role_user);
 
         $user = User::where('name', '=', 'Hank Robinson')->first()->id;
-        $role = Role::where('name', '=', 'staff')->first()->id;
+        $role = Role::where('name', '=', 'admin')->first()->id;
         $role_user = [ ['role_id' => $role, 'user_id' => $user, 'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create() ] ];
         DB::table('role_user')->insert($role_user);
 
         $user = User::where('name', '=', 'Lindsey Bandow')->first()->id;
-        $role = Role::where('name', '=', 'staff')->first()->id;
+        $role = Role::where('name', '=', 'admin')->first()->id;
         $role_user = [ 'role_id' => $role, 'user_id' => $user, 'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()  ];
         DB::table('role_user')->insert($role_user);
     }
@@ -91,8 +91,8 @@ class UsersRolesPermissions extends Seeder
         $adminRole = Role::where('name', '=', 'admin')->first();
         $adminRole->attachPermissions(array($manageUsers, $manageRoles));
 
-        $studentRole = Role::where('name', '=', 'staff')->first();
-        $studentRole->attachPermissions(array($readonlyAll));
+        // $studentRole = Role::where('name', '=', 'staff')->first();
+        // $studentRole->attachPermissions(array($readonlyAll));
     }
 }
 
