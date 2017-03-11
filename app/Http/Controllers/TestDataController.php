b6203fbde82034eebfa4f224ce280f6a623bd15c
mpdemink <mdemink@unomaha.edu>
2017-03-11 14:40:53 -0600

    passing json object from controller to test visualization

--- app/Http/Controllers/TestDataController.php
+++ app/Http/Controllers/TestDataController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Graduation;
 
 class TestDataController extends Controller
 {
     public function index()
     {
-    	return view('data.test');
+        $test_data = Graduation::select('school_id','GradRate4yr_BacDgr100','GradRate6yr_BacDgr150')->whereIn('school_id',[2,3,4,5,6])->get();
+        $this->viewData['test_data'] = json_encode($test_data);
+    	return view('data.test',$this->viewData);
+    	//return view('data.test');
+    	//return view('data.test_mpd');
     }
+
+
 }
