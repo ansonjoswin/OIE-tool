3efcfa7589b7865b583cac9460e3640dde6873d1
mpdemink <mdemink@unomaha.edu>
2017-03-16 22:25:53 -0500

    datavisual view displays resources and performance drop downs

--- app/Http/Controllers/DataVisualController.php
+++ app/Http/Controllers/DataVisualController.php
@@ -10,8 +10,49 @@ use DB;
 use Illuminate\Routing\Controller;
 
 
+
+
 class DataVisualController extends Controller
 {
+    public function __construct()
+    {
+        $xaxis_options = collect([''=>'Select Resource', 
+            'inst'=>'Instructors',
+            'inst_1000'=>'Instructors per Thousand Students',
+            'admin'=>'Admin and Professional Staff',
+            'admin_1000'=>'Admin and Professional Staff per Thousand Students',
+            'noninst'=>'Non-Instruction Academic Staff',
+            'noninst_1000'=>'Non-Instruction Acadmic Staff per Thousand Students',
+            'nonadmin'=>'Non-Admin Trade and Service Staff',
+            'nonadmin_1000'=>'Non-Admin Trade and Service Staff per Thousand Students',
+            'all'=>'All Instructors and Staff',
+            'undergrad_1000'=>'Undergrad Students per Thousand Students',
+            'totl_salary'=>'Total Salary',
+            'admin_salary'=>'Admin Salary',
+            'inst_salary'=>'Instructor Salary',
+            'inst_salary_mill'=>'Instructor Salary per Million',
+            'admin_staff_salary_mill'=>'Admin and Professional Staff Salary per Million',
+            'noninst_salary_mill'=>'Non-Instruction Academic Staff Salary per Million',
+            'nonadmin_salary_mill'=>'Non-Admin Trade and Service Staff Salary per Million'
+        ]);
+        $this->xaxis_options = $xaxis_options;
+
+        $yaxis_options = collect([''=>'Select Performance',
+            'avg_sch_undergrad'=>'Average SCH per Student per AY (undergrad)',
+            'avg_sch_grad'=>'Average SCH per Student per AY (graduate)',
+            'undergrad_degrees_1000'=>'Undergrad Degrees per Thousand Undergrad Students',
+            'undergrad_cert_1000'=>'Undergrad Certificates per Thousand Undergrad Students',
+            'grad_degrees_1000'=>'Graduate Degrees per Thousand Graduate Students', 
+            'grad_cert_1000'=>'Graduate Certificates per Thousand Graduate Students',
+            'bach_grad_rate_4'=>'Bachelor Degree 4-Yr Graduation Rate', 
+            'bach_grad_rate_6'=>'Bachelor Degree 6-Yr Graduation Rate', 
+            'assoc_grad_rate_4'=>'Associate Degree and Certificate 3-Yr Graduation Rate',
+            'default_rate'=>'Default Rate' 
+        ]);
+        $this->yaxis_options = $yaxis_options;
+    }
+
+
     public function index()
     {
     	$peerGroups = PeerGroup::where('PriPubFlg','=','public')->pluck('PeerGroupName','PeerGroupID')->toArray();
@@ -19,6 +60,8 @@ class DataVisualController extends Controller
 
         $test_data = '';
         $this->viewData['test_data'] = json_encode($test_data);
+        $this->viewData['xaxis_options'] = $this->xaxis_options;
+        $this->viewData['yaxis_options'] = $this->yaxis_options;
     	return view('data.index',$this->viewData);
     }
 
@@ -31,8 +74,14 @@ class DataVisualController extends Controller
     	$sel_year = $request['sel_year'];
     	$this->viewData['sel_year'] = $sel_year;
 
+        $this->viewData['xaxis_options'] = $this->xaxis_options;
+        $this->viewData['yaxis_options'] = $this->yaxis_options;        
+
     	$sel_xaxis = $request['sel_xaxis'];
     	$this->viewData['sel_xaxis'] = $sel_xaxis;
+
+        $sel_yaxis = $request['sel_yaxis'];
+        $this->viewData['sel_yaxis'] = $sel_yaxis;        
 
     	$peerGroups = PeerGroup::where('PriPubFlg','=','public')->pluck('PeerGroupName','PeerGroupID')->toArray();
     	$this->viewData['peerGroups'] = $peerGroups;
@@ -40,7 +89,6 @@ class DataVisualController extends Controller
         $test_data = Graduation::select('school_id','GradRate4yr_BacDgr100','GradRate6yr_BacDgr150')
         	->whereIn('school_id',[$sel_school_ids])
         	->get();
-
         $this->viewData['test_data'] = json_encode($test_data);
     	return view('data.index',$this->viewData);
     }    
--- resources/views/data/index.blade.php
+++ resources/views/data/index.blade.php
@@ -14,101 +14,6 @@ svg:not(:root) {
   shape-rendering: crispEdges;
 }
 
-.form-control2
-{
-    background-color: #333; 
-    color: #ddd;
-}
-
-article.accordion
-{
-    display: block;
-    width: 10em;
-    margin: 0 auto;
-    overflow: auto;
-    border-radius: 5px;
-}
-
-article.accordion section
-{
-    position: relative;
-    display: block;
-    float: left;
-    width: 2em;
-    height: 14em;
-    margin: 0.5em 0 0.5em 0.5em;
-    color: #333;
-    background-color: #333;
-    overflow: hidden;
-    border-radius: 3px;
-}
-
-article.accordion section h2
-{
-    position: absolute;
-    font-size: 1em;
-    font-weight: bold;
-    width: 12em;
-    height: 2em;
-    top: 14em;
-    left: 0;
-    text-indent: 1em;
-    padding: 0;
-    margin: 0;
-    color: #ddd;
-    -webkit-transform-origin: 0 0;
-    -moz-transform-origin: 0 0;
-    -ms-transform-origin: 0 0;
-    -o-transform-origin: 0 0;
-    transform-origin: 0 0;
-    -webkit-transform: rotate(-90deg);
-    -moz-transform: rotate(-90deg);
-    -ms-transform: rotate(-90deg);
-    -o-transform: rotate(-90deg);
-    transform: rotate(-90deg);
-}
-
-article.accordion section h2 a
-{
-    display: block;
-    width: 100%;
-    line-height: 2em;
-    text-decoration: none;
-    color: inherit;
-    outline: 0 none;
-}
-
-article.accordion section:target
-{
-    width: 30em;
-    padding: 0 1em;
-    color: #333;
-    background-color: #fff;
-}
-
-article.accordion section:target h2
-{
-    position: static;
-    font-size: 1.3em;
-    text-indent: 0;
-    color: #333;
-    -webkit-transform: rotate(0deg);
-    -moz-transform: rotate(0deg);
-    -ms-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-}
-
-article.accordion section,
-article.accordion section h2
-{
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    transition: all 1s ease;
-}
-
 .dot {
   stroke: #000;
 }
@@ -134,20 +39,20 @@ article.accordion section h2
                     <div class="panel-heading">
                         <div><h4>Data Visualization</h4></div>
                     </div>                   
-           
+
                     <div class="panel-body">
                         {!! Form::open(['url'=>'/datarefresh', 'class'=>'form-horizontal']) !!}
                         <div class="row">
                             <div class="col-md-10">
                                 <div class="panel-body">
-                                    <div class="col-md-5">
+                                    <div class="col-md-4">
                                     @if(isset($sel_pgid))
                                         {!! Form::select('sel_pgid', [''=>'Select Peer Group'] + $peerGroups, $sel_pgid, ['class'=>'form-control', 'required'=>'required']) !!}
                                     @else
                                         {!! Form::select('sel_pgid', [''=>'Select Peer Group'] + $peerGroups, null, ['class'=>'form-control', 'required'=>'required']) !!}
                                     @endif
                                     </div>
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                     @if(isset($sel_year))
                                         {!! Form::select('sel_year', [''=>'Select Year', '2012'=>'2012','2013'=>'2013','2014'=>'2014'], $sel_year, ['class'=>'form-control', 'required'=>'required']) !!}
                                     @else
@@ -155,41 +60,29 @@ article.accordion section h2
                                     @endif
                                     </div>
                                     <div class="col-md-2">
+                                    @if(isset($sel_xaxis))
+                                        {!! Form::select('sel_xaxis', $xaxis_options, $sel_xaxis, ['class'=>'form-control', 'required'=>'required']) !!}
+                                    @else
+                                        {!! Form::select('sel_xaxis', $xaxis_options, null, ['class'=>'form-control', 'required'=>'required']) !!}
+                                    @endif                                    
+                                    </div>
+                                    <div class="col-md-2">
+                                    @if(isset($sel_yaxis))
+                                        {!! Form::select('sel_yaxis', $yaxis_options, $sel_yaxis, ['class'=>'form-control', 'required'=>'required']) !!}
+                                    @else
+                                        {!! Form::select('sel_yaxis', $yaxis_options, null, ['class'=>'form-control', 'required'=>'required']) !!}
+                                    @endif                                    
+                                    </div>                                    
+                                    <div class="col-md-2">
                                     {!! Form::button('<i class="fa fa-btn"></i>Refresh', ['type' => 'submit', 'class' => 'btn btn-primary']) !!} 
                                     </div> 
                                 </div>                    
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-1">
-<!--                                 <article class="accordion">
-                                    <section id="acc1">
-                                        <h2><a href="#acc1">Select Performance</a></h2>
-                                        <h2>Select Performance</h2>
-                                        <p>
-                                        {{--
-                                        {!! Form::select('sel_year', [''=>'Select Performance', '2012'=>'Grad Rate 4 Year','2013'=>'Grad Rate 6 Year'], null, ['class'=>'form-control', 'required'=>'required']) !!}
-                                        --}}
-                                        </p>
-                                    </section>                         
-                                </article>  -->
-                            </div>                       
-                            <div class="col-md-7">
+                        <div class="row">               
+                            <div class="col-md-10 col-md-offset-1">
                                 @include('common.flash')
                                 <div id="chart">  </div>                        
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-10" >
-                                <div class="col-md-2" ></div>
-                                <div class="col-md-5" >
-                                @if(isset($sel_xaxis))
-                                    {!! Form::select('sel_xaxis', [''=>'Select Resource', 'Inst_Cnt'=>'Instructor Count','Admin_Cnt'=>'Admin Count','Emp_Cnt'=>'Employee Count'], $sel_xaxis, ['class'=>'form-control2', 'required'=>'required']) !!}
-                                @else
-                                    {!! Form::select('sel_xaxis', [''=>'Select Resource', 'Inst_Cnt'=>'Instructor Count','Admin_Cnt'=>'Admin Count','Emp_Cnt'=>'Employee Count'], null, ['class'=>'form-control2', 'required'=>'required']) !!}
-                                @endif
-                                </div>
-                                <div class="col-md-3" ></div>
                             </div>
                         </div>
                         {!! Form::close() !!}
