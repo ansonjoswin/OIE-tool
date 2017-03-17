740685afa0730b617c9f36d414a73356137aa30f
mpdemink <mdemink@unomaha.edu>
2017-03-16 23:18:48 -0500

    peergroup drop down includes public and private logic

--- app/Http/Controllers/DataVisualController.php
+++ app/Http/Controllers/DataVisualController.php
@@ -8,14 +8,14 @@ use App\PeerGroup;
 use App\School_PeerGroup;
 use DB;
 use Illuminate\Routing\Controller;
-
-
+use Auth;
 
 
 class DataVisualController extends Controller
 {
     public function __construct()
     {
+        //Defines resource drop down list
         $xaxis_options = collect([''=>'Select Resource', 
             'inst'=>'Instructors',
             'inst_1000'=>'Instructors per Thousand Students',
@@ -37,6 +37,7 @@ class DataVisualController extends Controller
         ]);
         $this->xaxis_options = $xaxis_options;
 
+        //Defines performance drop down list
         $yaxis_options = collect([''=>'Select Performance',
             'avg_sch_undergrad'=>'Average SCH per Student per AY (undergrad)',
             'avg_sch_grad'=>'Average SCH per Student per AY (graduate)',
@@ -53,43 +54,75 @@ class DataVisualController extends Controller
     }
 
 
+    private static function getPeerGroups()
+    {
+        //If logged in, user can see public and private peergroups
+        if (Auth::user() != null) {
+            $peerGroups = PeerGroup::where('PriPubFlg','=','public')
+                ->orWhere('User_ID', '=', Auth::user()->id)
+                ->pluck('PeerGroupName','PeerGroupID')->toArray();
+        //If not logged in, user can only see public peergroups
+        }else{
+            $peerGroups = PeerGroup::where('PriPubFlg','=','public')
+                ->pluck('PeerGroupName','PeerGroupID')->toArray();
+        }
+        return $peerGroups;        
+    }
+
+
     public function index()
     {
-    	$peerGroups = PeerGroup::where('PriPubFlg','=','public')->pluck('PeerGroupName','PeerGroupID')->toArray();
-    	$this->viewData['peerGroups'] = $peerGroups;
+        //Get list of available peerGroups
+    	$this->viewData['peerGroups'] = $this->getPeerGroups();
 
-        $test_data = '';
-        $this->viewData['test_data'] = json_encode($test_data);
+        //Get list of available resource and performance drop down options
         $this->viewData['xaxis_options'] = $this->xaxis_options;
         $this->viewData['yaxis_options'] = $this->yaxis_options;
+
+        //Get scatterplot data (empty on first load)
+        $test_data = '';
+        $this->viewData['test_data'] = json_encode($test_data);
+
+        //Call view
     	return view('data.index',$this->viewData);
     }
 
+
     public function refresh(Request $request)
     {
-    	$sel_pgid = $request['sel_pgid'];
-    	$sel_school_ids = DB::table('school_peergroups')->whereIn('PeerGroupID', [$sel_pgid])->pluck('School_ID');
-    	$this->viewData['sel_pgid'] = $sel_pgid;
+        //Get list of available peerGroups
+        $this->viewData['peerGroups'] = $this->getPeerGroups();
 
+        //Get list of available years
     	$sel_year = $request['sel_year'];
     	$this->viewData['sel_year'] = $sel_year;
 
+        //Get list of available resource and performance drop down options
         $this->viewData['xaxis_options'] = $this->xaxis_options;
         $this->viewData['yaxis_options'] = $this->yaxis_options;        
 
+        //Get selected resource parameter
     	$sel_xaxis = $request['sel_xaxis'];
     	$this->viewData['sel_xaxis'] = $sel_xaxis;
 
+        //Get selected performance parameter
         $sel_yaxis = $request['sel_yaxis'];
         $this->viewData['sel_yaxis'] = $sel_yaxis;        
 
-    	$peerGroups = PeerGroup::where('PriPubFlg','=','public')->pluck('PeerGroupName','PeerGroupID')->toArray();
-    	$this->viewData['peerGroups'] = $peerGroups;
+        //Get list of schools based on selected peerGroup
+        $sel_pgid = $request['sel_pgid'];
+        $sel_school_ids = DB::table('school_peergroups')
+            ->whereIn('PeerGroupID', [$sel_pgid])
+            ->pluck('School_ID');
+        $this->viewData['sel_pgid'] = $sel_pgid;
 
+        //Get scatterplot data based on list of schools
         $test_data = Graduation::select('school_id','GradRate4yr_BacDgr100','GradRate6yr_BacDgr150')
         	->whereIn('school_id',[$sel_school_ids])
         	->get();
         $this->viewData['test_data'] = json_encode($test_data);
+
+        //Call view
     	return view('data.index',$this->viewData);
     }    
 }
