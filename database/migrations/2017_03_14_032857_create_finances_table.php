d188e6a755a2b9c7e72067672cfaa6bf8f6c3ac7
amathur24 <amathur@unomaha.edu>
2017-04-01 23:09:53 -0600

    changes required in migration files

--- database/migrations/2017_03_14_032857_create_finances_table.php
+++ database/migrations/2017_03_14_032857_create_finances_table.php
@@ -25,67 +25,12 @@ class CreateFinancesTable extends Migration
             $table->string('created_by')->default('System');
             $table->string('updated_by')->default('System');
             $table->timestamp('created_at')->useCurrent();
-			      $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
             $table->softDeletes();
             $table->foreign('school_id')->references('id')->on('schools')->onUpdate('cascade')->onDelete('cascade');
             //$table->primary(['school_id']);
 
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('finances');
-    }
 }
-=======
-<?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
-class CreateFinancesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('finances', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('school_id')->index();
-            $table->integer('year')->index();
-            $table->bigInteger('public_total_salary_wage')->nullable();
-            $table->bigInteger('privateprofit_total_salary_wage')->nullable();
-            $table->bigInteger('private_nonprofit_total_salary_wage')->nullable();
-            $table->unique(['school_id', 'year']);
-            $table->string('created_by')->default('System');
-            $table->string('updated_by')->default('System');
-            $table->timestamp('created_at')->useCurrent();
-			      $table->timestamp('updated_at')->useCurrent();
-            $table->softDeletes();
-            $table->foreign('school_id')->references('id')->on('schools')->onUpdate('cascade')->onDelete('cascade');
-            //$table->primary(['school_id']);
-
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('finances');
-    }
-}
->>>>>>> bb1f73394331ba5039f02e1763aeb332ecf795ec
