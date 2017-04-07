5f51615e7c4098a4728b2f75406ffc812031354b
nanumula <nanumula@unomaha.edu>
2017-04-06 20:54:33 -0500

    change in carnegie migration script
    changed data type from float to integer

---
+++ database/migrations/2017_03_14_032848_create_carnegie_classifications_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarnegieClassificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carnegie_classifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('school_id')->index();
+            $table->integer('year')->index();
+            $table->integer('carnegie_basic')->nullable();
+            $table->integer('carnegie_ug_program')->nullable();
+            $table->integer('carnegie_grad_program')->nullable();
+            $table->integer('carnegie_ug_profile')->nullable();
+            $table->integer('carnegie_enroll_profile')->nullable();
+            $table->char('carnegie_size_setting')->nullable();
+            $table->integer('carnegie_classification2000')->nullable();
+            $table->unique(['school_id', 'year']);
+            $table->string('created_by')->default('System');
+            $table->string('updated_by')->default('System');
+            $table->timestamp('created_at')->useCurrent();
+			      $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+            $table->foreign('school_id')->references('id')->on('schools')->onUpdate('cascade')->onDelete('cascade');
+          //  $table->primary(['school_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('carnegie_classifications');
+    }
+}
