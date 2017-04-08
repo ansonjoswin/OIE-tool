ff648e8ef369d1c062b0de26ab03bc4722369577
nanumula <nanumula@unomaha.edu>
2017-04-07 19:39:54 -0500

    migration script for maps table

---
+++ database/migrations/2017_02_12_194501_create_maps_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMapsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maps', function (Blueprint $table) {
+            $table->increments('maps_id');
+            $table->string('column_header');
+            $table->string('csv_header')->nullable();
+            $table->string('table_name')->nullable();
+            $table->string('filename')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->foreign('table_name')->references('table_name')->on('map_tables');
+            $table->foreign('filename')->references('filename')->on('map_tables');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('maps');
+    }
+}
